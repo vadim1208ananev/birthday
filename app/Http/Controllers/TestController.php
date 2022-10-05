e534b52f04d8c4f61c2c7c54097b7702a428fa48
Vadim <vadim08ananev@gmail.com>
2022-10-05 17:54:10 +0300

    test work api

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,28 +7,39 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function index(){
+        $insert_scope='https://www.googleapis.com/auth/spreadsheets';
+       $read_scope=\Google_Service_Sheets::SPREADSHEETS;
+
         $google_creds=storage_path('credentials.json');
         putenv( 'GOOGLE_APPLICATION_CREDENTIALS=' . $google_creds);
         $client = new \Google_Client();
         $client->useApplicationDefaultCredentials();
 
-
-//dd(is_file(storage_path('credentials.json')));
-        // Области, к которым будет доступ
-        // https://developers.google.com/identity/protocols/googlescopes
-        $client->addScope( 'https://www.googleapis.com/auth/spreadsheets' );
+        $client->addScope( $read_scope );
 
         $service = new \Google_Service_Sheets($client);
         $spreadsheetId = '1IDBAp_r1cNbiCiK0WohkX2sczTHPFkHz4KjTj0Yu_Yc';
-        $sheetName = 'Sheet1';
+        $sheetName = 'Sheet11';
+        $range = $sheetName;
+        $doc=$service->spreadsheets_values->get($spreadsheetId, $range);
+        return response()->json($doc);
+       // print_r($doc);
 
         //clear all data
-        $range = $sheetName;  // TODO: Update placeholder value.
+     /*   $range = $sheetName;  // TODO: Update placeholder value.
         $requestBody = new \Google_Service_Sheets_ClearValuesRequest();
         $service->spreadsheets_values->clear($spreadsheetId, $range, $requestBody);
 
         // insert data
         $valueRange = new \Google_Service_Sheets_ValueRange();
+   $values=[[4,8,5]];
+        $body = new \Google_Service_Sheets_ValueRange( [ 'values' => $values] );
+
+        // valueInputOption - определяет способ интерпретации входных данных
+        // https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
+        // RAW | USER_ENTERED
+        $options = array( 'valueInputOption' => 'RAW' );
+       $service->spreadsheets_values->update( $spreadsheetId, $sheetName . '!A1', $body, $options );*/
     }
     //
 }
